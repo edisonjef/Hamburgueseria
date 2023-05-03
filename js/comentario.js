73b743c14461bf5beadad23e893f4350b668adf8
edison <edisonjef@gmail.com>
2023-05-03 19:26:31 -0300

    se elimina correo.py, comentario.js

--- js/comentario.js
+++
@@ -1,5 +0,0 @@
-let liMail = document.getElementById('liMail');
-liMail.textContent ="Email: " + localStorage.getItem('email').toString();
-
-let licomentario = document.getElementById('liComentario');
-licomentario.textContent ="Comentario: "+ localStorage.getItem('coment').toString();
