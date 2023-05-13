29b91701c0f0ee890b6787e0a786012f51a5bbd7
jorware <jorware@hotmail.com>
2023-05-13 17:28:49 -0300

    arreglado menu pedido con botones vaciar y pedir.

--- js/menu.js
+++ js/menu.js
@@ -15,6 +15,7 @@ var button10 = document.getElementById('10');
 var button11 = document.getElementById('11');
 var button12 = document.getElementById('12');
 var vaciar = document.getElementById('vaciar');
+var pedir = document.getElementById('pedir');
 
 // Creo un array vacio de 12 elementos
 var pedido = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -125,138 +126,187 @@ vaciar.addEventListener('click', function () {
     pedido[i] = 0;
   }
   //oculta el div de pedido
+
   var div = document.getElementById('pedido_activo');
   div.style.display = "none";
 
   console.log(pedido);
-  console.log('boton vaciar carrito');
+  console.log('boton vaciar carrito')});
+
+  pedir.addEventListener('click', function () {
+
+  console.log('boton pedir');
   cargaDePedido();
   window.location.href = "/formEnvioPedido.html";
-  
+  });
 
-});
-
-
-
-
-console.log(pedido);
 
 function muestraMenu() {
   // Muestra la ventana de pedido_activo
   var div = document.getElementById('pedido_activo');
-  div.style.display = 'block'; // Al activarse el evento muestra el div
-
+  // Al activarse el evento muestra el div
+  div.style.display = "grid";
   if (pedido[0] != 0) {
-    var mensaje = "Hamburguesa con queso X ";
+    var mensaje = "Hamburguesa con queso";
     var cantidad = pedido[0];
     var div = document.getElementById('pedido1');
-    div.innerHTML = mensaje + cantidad;
+    div.innerHTML = mensaje;
+    var div = document.getElementById('cant1');
+    div.innerHTML = cantidad;
   } else {
+    // Borra contenido de los divs producto y cantidad
     var div = document.getElementById('pedido1');
+    div.innerHTML = "";
+    var div = document.getElementById('cant1');
     div.innerHTML = "";
   }
 
 
   if (pedido[1] != 0) {
-    var mensaje = "Hamburguesa doble con queso X ";
+    var mensaje = "Hamburguesa doble con queso";
     var cantidad = pedido[1];
     var div = document.getElementById('pedido2');
-    div.innerHTML = mensaje + cantidad;
+    div.innerHTML = mensaje;
+    var div = document.getElementById('cant2');
+    div.innerHTML = cantidad;
+
   }else {
       var div = document.getElementById('pedido2');
+      div.innerHTML = "";
+      var div = document.getElementById('cant2');
       div.innerHTML = "";
     }
 
   if (pedido[2] != 0) {
-    var mensaje = "Hamburguesa triple con queso X ";
+    var mensaje = "Hamburguesa triple con queso";
     var cantidad = pedido[2];
     var div = document.getElementById('pedido3');
-    div.innerHTML = mensaje + cantidad;
+    div.innerHTML = mensaje;
+    var div = document.getElementById('cant3');
+    div.innerHTML = cantidad;
   }else {
       var div = document.getElementById('pedido3');
+      div.innerHTML = "";
+      var div = document.getElementById('cant3');
       div.innerHTML = "";
     }
 
   if (pedido[3] != 0) {
-    var mensaje = "Hamburguesa con queso y bacon X ";
+    var mensaje = "Hamburguesa con queso y bacon";
     var cantidad = pedido[3];
     var div = document.getElementById('pedido4');
-    div.innerHTML = mensaje + cantidad;
+    div.innerHTML = mensaje;
+    var div = document.getElementById('cant4');
+    div.innerHTML = cantidad;
   }else {
       var div = document.getElementById('pedido4');
+      div.innerHTML = "";
+      var div = document.getElementById('cant4');
       div.innerHTML = "";
     }
 
   if (pedido[4] != 0) {
-    var mensaje = "Hamburguesa doble con queso y bacon X ";
+    var mensaje = "Hamburguesa doble con queso y bacon";
     var cantidad = pedido[4];
     var div = document.getElementById('pedido5');
-    div.innerHTML = mensaje + cantidad;
+    div.innerHTML = mensaje;
+    var div = document.getElementById('cant5');
+    div.innerHTML = cantidad;
   }else {
     var div = document.getElementById('pedido5');
+    div.innerHTML = "";
+    var div = document.getElementById('cant5');
     div.innerHTML = "";
   }
   if (pedido[5] != 0) {
-    var mensaje = "Hamburguesa triple con queso y bacon X ";
+    var mensaje = "Hamburguesa triple con queso y bacon";
     var cantidad = pedido[5];
     var div = document.getElementById('pedido6');
-    div.innerHTML = mensaje + cantidad;
+    div.innerHTML = mensaje;
+    var div = document.getElementById('cant6');
+    div.innerHTML = cantidad;
   }else {
     var div = document.getElementById('pedido6');
+    div.innerHTML = "";
+    var div = document.getElementById('cant6');
     div.innerHTML = "";
   }
   if (pedido[6] != 0) {
-    var mensaje = "Hamburguesa cuadruple con queso X ";
+    var mensaje = "Hamburguesa cuadruple con queso";
     var cantidad = pedido[6];
     var div = document.getElementById('pedido7');
-    div.innerHTML = mensaje + cantidad;
+    div.innerHTML = mensaje;
+    var div = document.getElementById('cant7');
+    div.innerHTML = cantidad;
   }else {
     var div = document.getElementById('pedido7');
+    div.innerHTML = "";
+    var div = document.getElementById('cant7');
     div.innerHTML = "";
   }
   if (pedido[7] != 0) {
-    var mensaje = "Hamburguesa cuadruple con queso, bacon y salsa script X ";
+    var mensaje = "Hamburguesa cuadruple con queso, bacon y salsa script";
     var cantidad = pedido[7];
     var div = document.getElementById('pedido8');
-    div.innerHTML = mensaje + cantidad;
+    div.innerHTML = mensaje;
+    var div = document.getElementById('cant8');
+    div.innerHTML = cantidad;
   }else {
     var div = document.getElementById('pedido8');
+    div.innerHTML = "";
+    var div = document.getElementById('cant8');
     div.innerHTML = "";
   }
   if (pedido[8] != 0) {
-    var mensaje = "Hamburguesa doble con queso y cebolla caramelizada X ";
+    var mensaje = "Hamburguesa doble con queso y cebolla caramelizada";
     var cantidad = pedido[8];
     var div = document.getElementById('pedido9');
-    div.innerHTML = mensaje + cantidad;
+    div.innerHTML = mensaje;
+    var div = document.getElementById('cant9');
+    div.innerHTML = cantidad;
   }else {
     var div = document.getElementById('pedido9');
+    div.innerHTML = "";
+    var div = document.getElementById('cant9');
     div.innerHTML = "";
   }
   if (pedido[9] != 0) {
-    var mensaje = "Hamburguesa con , bacon y huevo frito X ";
+    var mensaje = "Hamburguesa con , bacon y huevo frito";
     var cantidad = pedido[9];
     var div = document.getElementById('pedido10');
-    div.innerHTML = mensaje + cantidad;
+    div.innerHTML = mensaje;
+    var div = document.getElementById('cant10');
+    div.innerHTML = cantidad;
   }else {
     var div = document.getElementById('pedido10');
+    div.innerHTML = "";
+    var div = document.getElementById('cant10');
     div.innerHTML = "";
   }
   if (pedido[10] != 0) {
-    var mensaje = "Hamburguesa doble con queso, bacon, huevo, lechuga y tomate X ";
+    var mensaje = "Hamburguesa doble con queso, bacon, huevo, lechuga y tomate";
     var cantidad = pedido[10];
     var div = document.getElementById('pedido11');
-    div.innerHTML = mensaje + cantidad;
+    div.innerHTML = mensaje;
+    var div = document.getElementById('cant11');
+    div.innerHTML = cantidad;
   }else {
     var div = document.getElementById('pedido11');
     div.innerHTML = "";
+    var div = document.getElementById('cant11');
+    div.innerHTML = "";
   }
   if (pedido[11] != 0) {
-    var mensaje = "Hamburguesa triple HTML: Huevo, Tomate, Mayonesa, Lechuga X ";
+    var mensaje = "Hamburguesa triple HTML: Huevo, Tomate, Mayonesa, Lechuga";
     var cantidad = pedido[11];
     var div = document.getElementById('pedido12');
-    div.innerHTML = mensaje + cantidad;
+    div.innerHTML = mensaje;
+    var div = document.getElementById('cant12');
+    div.innerHTML = cantidad;
   }else {
     var div = document.getElementById('pedido12');
+    div.innerHTML = "";
+    var div = document.getElementById('cant12');
     div.innerHTML = "";
   }
 
@@ -271,6 +321,7 @@ function muestraMenu() {
 // parte de Edison
 
 // captura de los elementos del pedido
+
 var divPedidoActivo = document.getElementById('pedido_activo').getElementsByTagName('div')
 sessionStorage.clear()
 sessionStorage.setItem('numeroPedido',(Math.floor(Math.random()*302)));
@@ -294,14 +345,14 @@ function cargaDePedido() {
 
             pedidoAenviar = pedidoAenviar + "<br>" + divPedidoActivo[i].innerText
         }
-        
+
         sessionStorage.setItem('pedido', pedidoAenviar);
         console.log(sessionStorage.getItem('pedido'))
 
 
     }
-    
-    
+
+
 
 
 
